beff637b64d12b93954746d41e6c0620fba9389a
Harry <60017963+hazzakins@users.noreply.github.com>
2025-12-24 01:25:32 +0000

    Correcting location

--- domains/domain-transfer.go
+++ domains/domain-transfer.go
@@ -1,6 +1,21 @@
 package domains
 
-// TODO: PE_GetTLDID
+import (
+	"github.com/hazzakins/go-enomapi/internal"
+	"github.com/hazzakins/go-enomapi/response"
+)
+
+// Upstream documentation: https://api.enom.com/docs/pe-get-tld-id
+func (c *Client) PEGetTLDID(tld string) (*response.PEGetTLDID, error) {
+	resp := internal.PEGetTLDIDResponse{}
+
+	cmd := c.NewCommand("PE_GetTLDID")
+	cmd.AddParam("TLD", tld)
+
+	err := c.Execute(cmd, &resp)
+	return resp.Decode(), err
+}
+
 // TODO: PE_SetPricing
 // TODO: PushDomain
 // TODO: RefillAccount
